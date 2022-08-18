daae22244db14d24f69f805b54ef3ba15df133ac
BraxtonM <braxtoncoding@gmail.com>
2022-08-18 19:40:41 -0400

    Fixed a bug in the code not allowing the user to get a new random password, Added comments to JavaScript code, Created ReadMe file

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# UCBC3_PasswordGenerator
+# Braxton M | UCBC3_PasswordGenerator | README
+
+In this challenge, we were tasked with a list of acceptance criteria (featured below this section) that would fix the following issues with the JavaScript File:
+
+* Generating a new password for a new user
+* Allowing the user to choose what variables would be used in their password
+* Having the password be randomized based on the variables they choose
+* Allow the user to request more customized passwords without having to reload the page
+
+The original code of the assignment gave us the layout of the page without the logic of the function to generate the password
+
+* Need to define variables
+* Need to check that the password falls between 8 - 128 characters
+* Need to log the variables the user has selected and output a password
+* Need the ability to repeat the function
+
+## Acceptance Criteria
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+## Screenshots
+
+Screenshots follow the order of the code and define sections 
+
+* Screenshot 1: Defining Variables
+* ![Defining Variables](../UCBC3_PasswordGenerator/Assets/Screenshots/screenshot%201.JPG)
+* Screenshot 2: Making sure length of password meets length criteria / starts password
+* ![Password Variables](../UCBC3_PasswordGenerator/Assets/Screenshots/screenshot%202.JPG)
+* Screenshot 3: Checking to make sure the user inputs one accepted criteria / Registers the variable they are asking for to be included
+* ![Accepted Criteria](../UCBC3_PasswordGenerator/Assets/Screenshots/screenshot%203.JPG)
+* Screenshot 4: Returns the function and allows each place in the password to be randomized with the Math.floor and Math.random functions 
+* ![Password Ranomization](../UCBC3_PasswordGenerator/Assets/Screenshots/screenshot%204.JPG)
+* Screenshot 5: Function writePassword runs and clears the previous entry before it runs the generate password function
+* ![Password reset](Assets/Screenshots/screen%20shot%205.JPG)
+
+## Noteable Links
+
+ - [Github Profile](https://github.com/BrackyM)
+ - [Github Challenge 3 Repository](https://github.com/BrackyM/UCBC3_PasswordGenerator)
+ - [Challenge 1 Live Site](https://brackym.github.io/UCBC3_PasswordGenerator/)
+
+ ## Contributing Classmates
+
+Classmates that I collaborated with on completing the assignment and bug fixing for eachother
+
+ - [Sky Texier Github](https://github.com/skytexier)
+ - [Khiem Phung Github](https://github.com/phungxkhiem)
+ - [Ishi Anand Github](https://github.com/ianad389)
--- script.js
+++ script.js
@@ -1,83 +1,80 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); // getting the button
 const upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+const lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz"; //static variables , easier to manage in string
 const specialCharactersCase = "!@#$%^&*()_<>=/,.'";
 const numbersCase = "1234567890";
-var possibleCharacters = "";
+var possibleCharacters = ""; //empty string
 
+function generatePassword() { //function definition (creating not running)
+  var startPass = confirm("Create a password?"); //user first question
+  if (startPass == true) { 
+    var passwordLength = prompt("Enter the length of your password");
 
-function generatePassword() {
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert(
+        "Your Password needs to be longer than 8 characters or shorter than 128 characters"  // if password outside parameters, start from beginning
+      );
+      return generatePassword();
+    } else {
+      alert("Let's Begin your password");
+    }
+    var upperCase = confirm("Do you want uppercase letters?");
+    var lowerCase = confirm("Do you want lowercase letters?");  //asking user question and confirming true or false
+    var numbers = confirm("Do you want numbers?");
+    var specialCharacters = confirm("Do you want special characters?");
 
-    var startPass =confirm("Create a password?");
-    if (startPass == true) {
-        var passwordLength = prompt("Enter the length of your password");
-    
-        if (passwordLength < 8 || passwordLength > 128) {
-            alert ("Your Password needs to be longer than 8 characters or shorter than 128 characters");
-            return generatePassword();
-        }else {
-            alert("Let's Begin your password");
-        }
+    console.log(
+      passwordLength,   //self check in console
+      upperCase,
+      lowerCase,
+      numbers,
+      specialCharacters
+    );
+
+    if (
+      upperCase === false &&
+      lowerCase === false &&
+      numbers === false &&                //if user inputs nothing
+      specialCharacters === false
+    ) {
+      alert("Your Password must be one of these character types");
     }
 
+    if (upperCase == true) {
+      possibleCharacters = possibleCharacters + upperCaseCharacters;
+    }
 
-  
-  var upperCase = confirm("Do you want uppercase letters?");
-  var lowerCase = confirm("Do you want lowercase letters?");
-  var numbers = confirm("Do you want numbers?");
-  var specialCharacters = confirm("Do you want special characters?");
+    if (lowerCase == true) {
+      possibleCharacters = possibleCharacters + lowerCaseCharacters;
+    }
 
-  console.log(passwordLength, upperCase, lowerCase, numbers, specialCharacters);
+    if (numbers == true) {
+      possibleCharacters = possibleCharacters + numbersCase;
+    }
 
-  if (upperCase ==true) {
-    possibleCharacters = possibleCharacters + upperCaseCharacters;
+    if (specialCharacters == true) {
+      possibleCharacters = possibleCharacters + specialCharactersCase;
+    }
+
+    var password = "";  // function return value
+    for (var i = 0; i < passwordLength; i++) {
+      var randomIndex = Math.floor(Math.random() * possibleCharacters.length); //randomization of password
+      password += possibleCharacters[randomIndex]; 
+    }
+
+    return password;
   }
-
-  if (lowerCase ==true) {
-    possibleCharacters = possibleCharacters + lowerCaseCharacters;
-  }
-
-  if (numbers==true) {
-    possibleCharacters = possibleCharacters +  numbersCase;
-  }
-
-  if (specialCharacters==true) {
-    possibleCharacters = possibleCharacters + specialCharactersCase;
-  }
-
-  var password = "";
-  for (var i = 0; i < passwordLength; i++) {
-    var randomIndex = Math.floor(Math.random() * possibleCharacters.length);
-    password += possibleCharacters[randomIndex];
-  }
-
-  return password;
 }
-
-if (
-  upperCaseCharacters === false && lowerCaseCharacters === false && numbersCase === false && specialCharactersCase === false
-) {
-  alert("Your Password must be one of these character types")
-}
-
-var passwordOptions = {
-  length:length,
-  hasUpperCaseCharacters:hasUpperCaseCharacters,
-  hasLowerCaseCharacters:hasLowerCaseCharacters,
-  hasNumbersCase:hasNumbersCase,
-  hasSpecialCharactersCase:hasSpecialCharactersCase,
-};
-
-return passwordOptions
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  possibleCharacters = ""; //clearing previous entries
+  var password = generatePassword(); // function running
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); //on click function is called
